7c783128acbc8ad85519e3aa179ec5b11b870f54
Ricky Vaughn <rickyvaughn2@gmail.com>
2016-12-18 11:35:41 -0600

    Create i-mosquitto.sh

---
+++ i-mosquitto.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "updating"
+sudo apt update
+sudo apt upgrade
+echo "openSSH"
+sudo apt install openssh-server -y
+echo "mosquitto"
+sudo apt install mosquitto -y
+echo "mosquitto clients"
+sudo apt install mosquitto-clients -y
+echo "mosquitto conf"
+printf 'listener 1883\n listener 1884\n listener 1885\n listener 1886\n listener 1887\n listener 1888\n listener 3000\n listener 3001\n\n listener 9000\n protocol websockets' > /etc/mosquitto/conf.d/r2-mosquitto.conf
